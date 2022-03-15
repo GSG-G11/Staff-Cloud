b3e96d7ac5a8a8656f1950445ed60b4073e3914d
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-03-15 21:19:52 +0200

    finishing add post Relates #16

--- src/utils/jwt/index.js
+++ src/utils/jwt/index.js
@@ -1,4 +1,4 @@
-const { verify, sign } = require('jsonwebtoken');
+const { verify } = require('jsonwebtoken');
 
 const verifyToken = (token, secret) => new Promise((resolve, reject) => {
   verify(token, secret, (err, decode) => {
@@ -9,16 +9,6 @@ const verifyToken = (token, secret) => new Promise((resolve, reject) => {
   });
 });
 
-const signToken = (payload, secret) => new Promise((resolve, reject) => {
-  sign(payload, secret, (err, token) => {
-    if (err) {
-      err.status(401);
-      reject(err);
-    } else resolve(token);
-  });
-});
-
 module.exports = {
   verifyToken,
-  signToken,
 };
